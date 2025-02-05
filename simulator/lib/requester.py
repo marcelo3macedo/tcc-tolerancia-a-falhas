92ff902ece0fc31ba506a199454c01b9f540869f
Marcelo Macedo <marcelo3macedo@gmail.com>
2025-02-05 18:31:12 -0300

    feat: adding requests

--- simulator/lib/requester.py
+++ simulator/lib/requester.py
@@ -2,6 +2,7 @@ from prometheus_client import Counter, Histogram, start_http_server
 import requests, os, time
 from dotenv import load_dotenv
 from concurrent.futures import ThreadPoolExecutor
+from tenacity import retry, stop_after_attempt, wait_exponential, retry_if_exception_type, RetryError
 
 start_http_server(8000)
 load_dotenv()
@@ -18,6 +19,16 @@ REQUEST_LATENCY = Histogram(
     ["method", "endpoint"]
 )
 
+def log_retry(retry_state):
+    attempt_number = retry_state.attempt_number
+    total_attempts = retry_state.kwargs.get('retries', 5)
+    print(f"Attempt {attempt_number}/{total_attempts} failed for {retry_state.args[0]}: {retry_state.outcome.exception()}")
+
+@retry(
+    stop=stop_after_attempt(5),
+    wait=wait_exponential(multiplier=1, min=1, max=16),
+    retry=retry_if_exception_type(requests.exceptions.RequestException)
+)
 def sendRequest(endpoint):
     start_time = time.time()
     url = f"{API_ENDPOINT}{endpoint}"
@@ -43,8 +54,22 @@ def sendRequest(endpoint):
 
         print(f"Error requesting {endpoint}: {e}")
 
+def safeSendRequest(endpoint, retries=5):
+    try:
+        sendRequest(endpoint, retries=retries)
+    except RetryError as retry_error:
+        REQUEST_COUNT.labels(
+            method="POST",
+            endpoint=endpoint,
+            status="error"
+        ).inc()
+
+        print(f"Error: Max retries reached for {endpoint}. Giving up.")
+        print(f"Final failure: {retry_error.last_attempt.exception()}")
+        raise retry_error.last_attempt.exception()
+
 def sendRequests(endpoint, count):
     with ThreadPoolExecutor(max_workers=count) as executor:
-        futures = [executor.submit(sendRequest, endpoint) for _ in range(count)]
+        futures = [executor.submit(safeSendRequest, endpoint) for _ in range(count)]
         for future in futures:
             future.result()
